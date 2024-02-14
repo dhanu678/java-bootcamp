a381a6faa2e72b0d7dd1ec322a7a734ac1b12d48
dhanashreechavan <dhanashreec8590.com>
2024-02-14 11:19:52 +0530

    added new feature

---
+++ day 5/assignment/booleanexample.java
@@ -0,0 +1,12 @@
+ class booleanexample {
+    public static void main(String[] args) {
+        
+        System.out.println((52 > 25) && (10 <= 8));
+        System.out.println((20 > 25 ) && (22 < 20) && (26 == 26));
+        System.out.println( (45 > 2) && ( 10 == 10));
+
+
+    }
+}
+        
+    
